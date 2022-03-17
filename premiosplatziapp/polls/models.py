f9332c46c1fbc03c3cedaae6e29e697416d786ab
mkangelo <mikequi000@gmail.com>
2022-03-16 23:33:34 -0600

    Modelos creados ORM

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    #Django por automatico tiene un id auto incrementado
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date  published")
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choise_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
