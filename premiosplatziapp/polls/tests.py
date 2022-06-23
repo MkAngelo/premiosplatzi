05c49ed37b1ec67c842fba9bf7b892df0ac01780
mkangelo <mikequi000@gmail.com>
2022-06-22 23:14:56 -0500

    New tests

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -16,3 +16,17 @@ class QuestionModelTests(TestCase):
 		future_question = Question(question_text="Quien es el mejor Course Director de Platzi?",pub_date=time)
 
 		self.assertIs(future_question.was_published_recently(), False)
+
+	def test_was_published_recently_with_present_questions(self):
+		"""was_published_recently returns True for questions whose pub_date is in the present"""
+		time = timezone.now()
+		present_question = Question(question_text="Quien es el mejor Course Director de Platzi?",pub_date=time)
+
+		self.assertIs(present_question.was_published_recently(), True)
+
+	def test_was_published_recently_with_past_questions(self):
+		"""was_published_recently returns False for questions whose pub_date is in the present"""
+		time = timezone.now() - datetime.timedelta(days=30)
+		past_question = Question(question_text="Quien es el mejor Course Director de Platzi?",pub_date=time)
+
+		self.assertIs(past_question.was_published_recently(), False)
