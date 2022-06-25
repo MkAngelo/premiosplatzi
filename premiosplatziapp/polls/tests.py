b9bf8aa66df0b214bf12b91394a65d58ede03db1
mkangelo <mikequi000@gmail.com>
2022-06-25 13:59:55 -0500

    Restricted Views

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -103,4 +103,24 @@ class QuestionIndexViewTests(TestCase):
 			[]
 		)
 
-		
+
+class QuestionDetailViewTests(TestCase):
+	def test_future_question(self):
+		"""
+		The detail view of a question with a pub_date in the future
+		return a 404 error not found
+		"""
+		future_question = create_question(question_text="Future Question", days=30)
+		url = reverse('polls:detail', args=(future_question.id,))
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 404)
+
+	def test_past_question(self):
+		"""
+		The detail view of a question with a pub_date int he past
+		displays the question's text
+		"""
+		past_question = create_question(question_text="Past Question", days=-30)
+		url = reverse('polls:detail', args=(past_question.id,))
+		response = self.client.get(url)
+		self.assertContains(response, past_question.question_text)
